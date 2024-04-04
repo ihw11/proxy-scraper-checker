577e84b7507cbd8593ab13e4f8335f5bc9bea833
ihw11 <122907907+ihw11@users.noreply.github.com>
2024-04-04 10:25:54 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,4 @@
 
 set -eu
 
-python3 -m venv --upgrade-deps .venv
-.venv/bin/python3 -m pip install -U --disable-pip-version-check --editable .[non-termux]
-.venv/bin/python3 -m proxy_scraper_checker
+python3 -m proxy_scraper_checker
